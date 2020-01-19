aeadc802e4a80f0fff463940524083cc459c6144
devplayg <devplayg@gmail.com>
2020-01-19 11:05:25 +0900

    added plugins

--- get.go
+++ get.go
@@ -4,11 +4,41 @@ import (
 	"github.com/devplayg/eggcrate"
 )
 
-const Bootstrap4 = "bs4"
+const (
+	Bootstrap4 = "bs4"
+	Plugins    = "plugins"
+)
 
-func GetAssetMap(assetType string) (map[string][]byte, error) {
-	if assetType == Bootstrap4 {
-		return eggcrate.Decode(bs4)
+func GetAssetMap(assets ...string) (map[string][]byte, error) {
+	assetMap := make(map[string][]byte, 0)
+	for i := range assets {
+		if assets[i] == Bootstrap4 {
+			if err := merge(bootstrap4, assetMap); err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		if assets[i] == Plugins {
+			if err := merge(plugins, assetMap); err != nil {
+				return nil, err
+			}
+			continue
+		}
+
 	}
-	return nil, nil
+	return assetMap, nil
+}
+
+func merge(data string, assetMap map[string][]byte) error {
+	m, err := eggcrate.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range m {
+		assetMap[k] = v
+	}
+
+	return nil
 }
---
+++ html_base.go
@@ -0,0 +1,233 @@
+package himma
+
+func Base() string {
+	// /assets/plugins/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css
+	// /assets/plugins/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js
+	// /assets/plugins/bootstrap-select/bootstrap-select.min.css
+	// /assets/plugins/bootstrap-select/bootstrap-select.min.js
+	// /assets/plugins/bootstrap-table/bootstrap-table-export.min.js
+	// /assets/plugins/bootstrap-table/bootstrap-table.min.css
+	// /assets/plugins/bootstrap-table/bootstrap-table.min.js
+	// /assets/plugins/bootstrap-table/tableExport.min.js
+	// /assets/plugins/holderjs/holder.min.js
+	// /assets/plugins/jquery-validation/additional-methods.min.js
+	// /assets/plugins/jquery-validation/jquery.validate.min.js
+	// /assets/plugins/jquery.mask/jquery.mask.min.js
+	// /assets/plugins/js.cookie/js.cookie-2.2.1.min.js
+	// /assets/plugins/moment/moment-timezone-with-data.min.js
+	// /assets/plugins/moment/moment-timezone.min.js
+	// /assets/plugins/moment/moment.min.js
+	// /assets/plugins/progressbar.js/progressbar.min.js
+	// /assets/plugins/sweetalert2/sweetalert2.min.css
+	// /assets/plugins/sweetalert2/sweetalert2.min.js
+	// /assets/plugins/videojs/video-js.min.css
+	// /assets/plugins/videojs/video.min.js
+	// /assets/plugins/videojs/videojs-http-streaming.min.js
+	// /assets/plugins/waitMe/waitMe.min.css
+	// /assets/plugins/waitMe/waitMe.min.js
+
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>
+        RTSP Stream
+    </title>
+    <meta name="description" content="Page Titile">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <!-- Call App Mode on ios devices -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <!-- Remove Tap Highlight on Windows Phone IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+    <!-- base css -->
+    <link rel="stylesheet" media="screen, print" href="assets/css/vendors.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="assets/css/app.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="assets/css/sweetalert2.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="assets/css/themes/cust-theme-3.css">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/logo.png">
+    <link rel="mask-icon" href="assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="stylesheet" media="screen, print" href="assets/css/fa-regular.css">
+    <link rel="stylesheet" media="screen, print" href="assets/css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="assets/plugins/bootstrap-table/bootstrap-table.min.css">
+<link rel="stylesheet" media="screen, print" href="assets/plugins/waitMe/waitMe.min.css">
+    {{ block "css" . }}{{ end }}
+</head>
+<body class="mod-bg-1 nav-function-fixed">
+<!-- BEGIN Page Wrapper -->
+<div class="page-wrapper">
+    <div class="page-inner">
+        <!-- BEGIN Left Aside -->
+        <aside class="page-sidebar">
+            <div class="page-logo">
+                <a href="https://github.com/devplayg/goback" class="page-logo-link press-scale-down d-flex align-items-center position-relative">
+                    <!--<img src="img/logo.png" alt="RTSP Stream" aria-roledescription="logo">-->
+                    <span class="page-logo-text mr-1">GoBack</span>
+                    <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
+                    <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
+                </a>
+            </div>
+            <!-- BEGIN PRIMARY NAVIGATION -->
+            <nav id="js-primary-nav" class="primary-nav" role="navigation">
+                <ul id="js-nav-menu" class="nav-menu">
+                    <li class="nav-title">Backup Software</li>
+                    <li>
+                        <a href="live.html" title="Live">
+                            <i class="far fa-video"></i>
+                            <span class="nav-link-text">Backup</span>
+                        </a>
+                    </li>
+                </ul>
+                <div class="filter-message js-filter-message bg-success-600"></div>
+            </nav>
+            <!-- END PRIMARY NAVIGATION -->
+        </aside>
+        <!-- END Left Aside -->
+        <div class="page-content-wrapper">
+            <!-- BEGIN Page Header -->
+            <header class="page-header" role="banner">
+                <!-- we need this logo when user switches to nav-function-top -->
+                <div class="page-logo">
+                    <a href="#" class="page-logo-link press-scale-down d-flex align-items-center position-relative">
+                        <img src="img/logo.png" alt="RTSP Stream" aria-roledescription="logo">
+                        <span class="page-logo-text mr-1">GoBack</span>
+                        <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
+                        <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
+                    </a>
+                </div>
+                <!-- DOC: mobile button appears during mobile width -->
+                <div class="hidden-lg-up">
+                    <a href="#" class="header-btn btn press-scale-down" data-action="toggle" data-class="mobile-nav-on">
+                        <i class="ni ni-menu"></i>
+                    </a>
+                </div>
+            </header>
+            <!-- END Page Header -->
+            <!-- BEGIN Page Content -->
+            <!-- the #js-page-content id is needed for some plugins to initialize -->
+            <main id="js-page-content" role="main" class="page-content">
+                <!-- Content ### -->
+{{ block "content" . }}{{ end }}
+    </main>
+            <!-- this overlay is activated only when mobile menu is triggered -->
+            <div class="page-content-overlay" data-action="toggle" data-class="mobile-nav-on"></div> <!-- END Page Content -->
+           
+            <!-- END Page Footer -->
+            <!-- BEGIN Color profile -->
+            <!-- this area is hidden and will not be seen on screens or screen readers -->
+            <!-- we use this only for CSS color refernce for JS stuff -->
+            <p id="js-color-profile" class="d-none">
+                <span class="color-primary-50"></span>
+                <span class="color-primary-100"></span>
+                <span class="color-primary-200"></span>
+                <span class="color-primary-300"></span>
+                <span class="color-primary-400"></span>
+                <span class="color-primary-500"></span>
+                <span class="color-primary-600"></span>
+                <span class="color-primary-700"></span>
+                <span class="color-primary-800"></span>
+                <span class="color-primary-900"></span>
+                <span class="color-info-50"></span>
+                <span class="color-info-100"></span>
+                <span class="color-info-200"></span>
+                <span class="color-info-300"></span>
+                <span class="color-info-400"></span>
+                <span class="color-info-500"></span>
+                <span class="color-info-600"></span>
+                <span class="color-info-700"></span>
+                <span class="color-info-800"></span>
+                <span class="color-info-900"></span>
+                <span class="color-danger-50"></span>
+                <span class="color-danger-100"></span>
+                <span class="color-danger-200"></span>
+                <span class="color-danger-300"></span>
+                <span class="color-danger-400"></span>
+                <span class="color-danger-500"></span>
+                <span class="color-danger-600"></span>
+                <span class="color-danger-700"></span>
+                <span class="color-danger-800"></span>
+                <span class="color-danger-900"></span>
+                <span class="color-warning-50"></span>
+                <span class="color-warning-100"></span>
+                <span class="color-warning-200"></span>
+                <span class="color-warning-300"></span>
+                <span class="color-warning-400"></span>
+                <span class="color-warning-500"></span>
+                <span class="color-warning-600"></span>
+                <span class="color-warning-700"></span>
+                <span class="color-warning-800"></span>
+                <span class="color-warning-900"></span>
+                <span class="color-success-50"></span>
+                <span class="color-success-100"></span>
+                <span class="color-success-200"></span>
+                <span class="color-success-300"></span>
+                <span class="color-success-400"></span>
+                <span class="color-success-500"></span>
+                <span class="color-success-600"></span>
+                <span class="color-success-700"></span>
+                <span class="color-success-800"></span>
+                <span class="color-success-900"></span>
+                <span class="color-fusion-50"></span>
+                <span class="color-fusion-100"></span>
+                <span class="color-fusion-200"></span>
+                <span class="color-fusion-300"></span>
+                <span class="color-fusion-400"></span>
+                <span class="color-fusion-500"></span>
+                <span class="color-fusion-600"></span>
+                <span class="color-fusion-700"></span>
+                <span class="color-fusion-800"></span>
+                <span class="color-fusion-900"></span>
+            </p>
+            <!-- END Color profile -->
+        </div>
+    </div>
+</div>
+<!-- END Page Wrapper -->
+<!-- BEGIN Quick Menu -->
+<!-- to add more items, please make sure to change the variable '$menu-items: number;' in your _page-components-shortcut.scss -->
+<nav class="shortcut-menu d-none d-sm-block">
+    <input type="checkbox" class="menu-open" name="menu-open" id="menu_open" />
+    <label for="menu_open" class="menu-open-button ">
+        <span class="app-shortcut-icon d-block"></span>
+    </label>
+    <a href="#" class="menu-item btn" data-toggle="tooltip" data-placement="left" title="Scroll Top">
+        <i class="fal fa-arrow-up"></i>
+    </a>
+    <a href="page_login_alt.html" class="menu-item btn" data-toggle="tooltip" data-placement="left" title="Logout">
+        <i class="fal fa-sign-out"></i>
+    </a>
+    <a href="#" class="menu-item btn" data-action="app-fullscreen" data-toggle="tooltip" data-placement="left" title="Full Screen">
+        <i class="fal fa-expand"></i>
+    </a>
+    <a href="#" class="menu-item btn" data-action="app-print" data-toggle="tooltip" data-placement="left" title="Print page">
+        <i class="fal fa-print"></i>
+	</a>
+	<a href="#" class="menu-item btn" data-action="app-voice" data-toggle="tooltip" data-placement="left" title="Voice command">
+	<i class="fal fa-microphone"></i>
+	</a>
+	</nav>
+	<!-- END Quick Menu -->
+	<script src="assets/js/vendors.bundle.js"></script>
+	<script src="assets/js/app.bundle.js"></script>
+	<script src="assets/js/sweetalert2.bundle.js"></script>
+	<script src="assets/plugins/bootstrap-table/bootstrap-table.min.js"></script>
+	<script src="assets/plugins/bootstrap-table/tableExport.min.js"></script>
+	<script src="assets/plugins/bootstrap-table/bootstrap-table-export.min.js"></script>
+	<script src="assets/plugins/jquery.mask.min.js"></script>
+	<script src="assets/plugins/js.cookie-2.2.1.min.js"></script>
+	<script src="assets/plugins/progressbar.js/progressbar.min.js"></script>
+	<script src="assets/plugins/moment/moment.min.js"></script>
+	<script src="assets/plugins/moment/moment-timezone.min.js"></script>
+	<script src="assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+	<script src="assets/plugins/waitMe/waitMe.min.js"></script>
+	<script>
+	$(document).ready(function () {
+
+	});
+	</script>
+	{{ block "script" . }}{{ end }}
+	</body>
+	</html>`
+}
