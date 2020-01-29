140a9c22c3b10d2394396ba2e32a9ec6cf3b87f9
devplayg <devplayg@gmail.com>
2020-01-29 10:12:37 +0900

    split all assets

--- asset/init.go
+++ asset/init.go
@@ -1,30 +1,31 @@
 package asset
 
-import "fmt"
+import (
+    "github.com/devplayg/himma"
+)
 
 var rawAssetMap map[string]*string
 
 func init() {
-    fmt.Println("HIMMA STARTED")
-	rawAssetMap = map[string]*string{
-        "bootstrap4":                &bootstrap4,
-        "bootstrapDatepicker_1_9_0": &bootstrapDatepicker_1_9_0,
-        "bootstrapSelect_1_13_9":    &bootstrapSelect_1_13_9,
-        "bootstrapTable_1_15_5":     &bootstrapTable_1_15_5,
-        "holder_2_9":                &holder_2_9,
-        "jqueryMask_1_14_16":        &jqueryMask_1_14_16,
-        "jqueryValidation_1_19_1":   &jqueryValidation_1_19_1,
-        "jsCookie_2_2_1":            &jsCookie_2_2_1,
-        "moment_2_24_0":             &moment_2_24_0,
-        "progressbarJs_1_0_1":       &progressbarJs_1_0_1,
-        "videoJs_7_7_4":             &videoJs_7_7_4,
-        "waitMe_31_10_17":           &waitMe_31_10_17,
+    rawAssetMap = map[string]*string{
+        himma.Bootstrap4:                &bootstrap4,
+        himma.BootstrapDatepicker_1_9_0: &bootstrapDatepicker_1_9_0,
+        himma.BootstrapSelect_1_13_9:    &bootstrapSelect_1_13_9,
+        himma.BootstrapTable_1_15_5:     &bootstrapTable_1_15_5,
+        himma.Holder_2_9:                &holder_2_9,
+        himma.JqueryMask_1_14_16:        &jqueryMask_1_14_16,
+        himma.JqueryValidation_1_19_1:   &jqueryValidation_1_19_1,
+        himma.JsCookie_2_2_1:            &jsCookie_2_2_1,
+        himma.Moment_2_24_0:             &moment_2_24_0,
+        himma.ProgressbarJs_1_0_1:       &progressbarJs_1_0_1,
+        himma.VideoJs_7_7_4:             &videoJs_7_7_4,
+        himma.WaitMe_31_10_17:           &waitMe_31_10_17,
     }
 }
 
 func GetRaw(key string) *string {
-	if val, have := rawAssetMap[key]; have {
-		return val
-	}
-	return nil
+    if val, have := rawAssetMap[key]; have {
+        return val
+    }
+    return nil
 }
