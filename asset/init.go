b57f10c31fecc784c7abc4305f561ca9f11de7ce
devplayg <devplayg@gmail.com>
2020-01-29 19:29:02 +0900

    fixed links

--- asset/init.go
+++ asset/init.go
@@ -1,25 +1,36 @@
 package asset
 
-import (
-    "github.com/devplayg/himma"
+const (
+    Bootstrap4                = "bootstrap4"
+    BootstrapDatepicker_1_9_0 = "bootstrapDatepicker_1_9_0"
+    BootstrapSelect_1_13_9    = "bootstrapSelect_1_13_9"
+    BootstrapTable_1_15_5     = "bootstrapTable_1_15_5"
+    Holder_2_9                = "holder_2_9"
+    JqueryMask_1_14_16        = "jqueryMask_1_14_16"
+    JqueryValidation_1_19_1   = "jqueryValidation_1_19_1"
+    JsCookie_2_2_1            = "jsCookie_2_2_1"
+    Moment_2_24_0             = "moment_2_24_0"
+    ProgressbarJs_1_0_1       = "progressbarJs_1_0_1"
+    VideoJs_7_7_4             = "videoJs_7_7_4"
+    WaitMe_31_10_17           = "waitMe_31_10_17"
 )
 
 var rawAssetMap map[string]*string
 
 func init() {
     rawAssetMap = map[string]*string{
-        himma.Bootstrap4:                &bootstrap4,
-        himma.BootstrapDatepicker_1_9_0: &bootstrapDatepicker_1_9_0,
-        himma.BootstrapSelect_1_13_9:    &bootstrapSelect_1_13_9,
-        himma.BootstrapTable_1_15_5:     &bootstrapTable_1_15_5,
-        himma.Holder_2_9:                &holder_2_9,
-        himma.JqueryMask_1_14_16:        &jqueryMask_1_14_16,
-        himma.JqueryValidation_1_19_1:   &jqueryValidation_1_19_1,
-        himma.JsCookie_2_2_1:            &jsCookie_2_2_1,
-        himma.Moment_2_24_0:             &moment_2_24_0,
-        himma.ProgressbarJs_1_0_1:       &progressbarJs_1_0_1,
-        himma.VideoJs_7_7_4:             &videoJs_7_7_4,
-        himma.WaitMe_31_10_17:           &waitMe_31_10_17,
+        Bootstrap4:                &bootstrap4,
+        BootstrapDatepicker_1_9_0: &bootstrapDatepicker_1_9_0,
+        BootstrapSelect_1_13_9:    &bootstrapSelect_1_13_9,
+        BootstrapTable_1_15_5:     &bootstrapTable_1_15_5,
+        Holder_2_9:                &holder_2_9,
+        JqueryMask_1_14_16:        &jqueryMask_1_14_16,
+        JqueryValidation_1_19_1:   &jqueryValidation_1_19_1,
+        JsCookie_2_2_1:            &jsCookie_2_2_1,
+        Moment_2_24_0:             &moment_2_24_0,
+        ProgressbarJs_1_0_1:       &progressbarJs_1_0_1,
+        VideoJs_7_7_4:             &videoJs_7_7_4,
+        WaitMe_31_10_17:           &waitMe_31_10_17,
     }
 }
 
--- get.go
+++ get.go
@@ -5,21 +5,6 @@ import (
 	"github.com/devplayg/himma/asset"
 )
 
-const (
-    Bootstrap4                = "bootstrap4"
-    BootstrapDatepicker_1_9_0 = "bootstrapDatepicker_1_9_0"
-    BootstrapSelect_1_13_9    = "bootstrapSelect_1_13_9"
-    BootstrapTable_1_15_5     = "bootstrapTable_1_15_5"
-    Holder_2_9                = "holder_2_9"
-    JqueryMask_1_14_16        = "jqueryMask_1_14_16"
-    JqueryValidation_1_19_1   = "jqueryValidation_1_19_1"
-    JsCookie_2_2_1            = "jsCookie_2_2_1"
-    Moment_2_24_0             = "moment_2_24_0"
-    ProgressbarJs_1_0_1       = "progressbarJs_1_0_1"
-    VideoJs_7_7_4             = "videoJs_7_7_4"
-    WaitMe_31_10_17           = "waitMe_31_10_17"
-)
-
 func GetAssetMap(assets ...string) (map[string][]byte, error) {
     assetMap := make(map[string][]byte, 0)
     for i := range assets {
