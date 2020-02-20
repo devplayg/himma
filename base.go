474b1dae83f1096201b35e6a68485d2673f413d2
devplayg <devplayg@gmail.com>
2020-02-21 00:26:41 +0900

    added source

--- base.go
+++ base.go
@@ -4,15 +4,16 @@ import (
 	"io/ioutil"
 )
 
-type Application struct {
-	AppName     string
-	Description string
-	Url         string
-	Phrase1     string
-	Phrase2     string
-	Year        int
-	Version     string
-	Company     string
+type Config struct {
+	AppName        string
+	Description    string
+	Url            string
+	Phrase1        string
+	Phrase2        string
+	Year           int
+	Version        string
+	Company        string
+	Logo           string
 }
 
 func Base() string {
